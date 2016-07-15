61aec50150e09d2909f5f3a612af9e1089b88fde
MartinN13 <martinnabhan@me.com>
2016-07-15 17:58:02 +0200

    Go back to CSS, implement Bootstrap 4 and use new card component in grid

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,9 @@
 $(document).ready(function() {
-  $(".search-input").click(function() {
-    $(".search-input").val("");
+  $(".form-control").click(function() {
+    $(".form-control").val("");
   });
 
-  $(".search-input").focusout(function() {
-    $(".search-input").val("Search Book");
-  });
-
-  function resizeCards() {
-    $(".book").height($(".book").width() / 1.75);
-
-    $(".book-cover").height($(".book").width() / 2.0132743363);
-    $(".book-cover").width($(".book-cover").height() / 1.614);
-    $(".book-image").width($(".book-cover").width());
-    $(".book-image").css("left", $(".book").width() / 22.75);
-
-    $(".book-info").css("left", 25 + parseFloat($(".book-image").css("left")) + 
-                        $(".book-cover").width() + parseFloat($(".book-image").css("left")) * 1.5);
-    $(".book-info").width($(".book-col").outerWidth() - parseFloat($(".book-info").css("left")) - 
-                          parseFloat($(".book-image").css("left")) * 1.5 - 25);
-  }
-
-  $(window).resize(function() {
-    resizeCards();
+  $(".form-control").focusout(function() {
+    $(".form-control").val("Search Book");
   });
 });
