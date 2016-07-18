a4618936a3cd08fa047700cf599fe89e17f1abcd
MartinN13 <martinnabhan@me.com>
2016-07-19 01:41:35 +0200

    Add font awesome, add more style to navbar, genre menu and grid

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-  $(".form-control").click(function() {
+  $(".form-control").mousedown(function() {
     $(".form-control").val("");
   });
 
   $(".form-control").focusout(function() {
     $(".form-control").val("Search Book");
+  });
+
+  $(".book-genre").each(function(){
+    if (this.href == document.location) {
+      jQuery(this).addClass("active");
+    }
   });
 });
