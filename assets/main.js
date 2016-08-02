6c5612d0031ce0e9528d2726e02f52bf0e8db698
MartinN13 <martinnabhan@me.com>
2016-08-02 22:18:16 +0200

    Start implementing carousel banner

--- assets/main.js
+++ assets/main.js
@@ -9,11 +9,23 @@ $(document).ready(function() {
 
   $(".book-genre").each(function(){
     if (this.href == document.location) {
-      jQuery(this).addClass("active");
+      jQuery(this).addClass("current");
     }
   });
 
   if ($(document.body).height() < $(window).height()) {
     $("footer").addClass("navbar-fixed-bottom");
   }
+
+  $('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    autoWidth: true,
+    stagePadding: 310,
+    loop: true,
+    margin: 1,
+    center: true,
+    dots: false,
+  });
 });
